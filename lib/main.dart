26a3c735b80240545ded6b7d1a3bb340624806f1
Ahmed Saleh <>
2021-03-03 14:16:48 +0200

    add theme and scaffold colors

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.blueGrey[900],
+      ),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+      ),
+    ),
+  );
+}
